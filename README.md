e47ae8daccd48f0374258adda6b276433b1696b8
Viv-bit <vivian.morenikenji@gmail.com>
2021-01-06 13:38:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Todolist App created with Ionic-Vue
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,20 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>TodoList</ion-title>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">TodoList</ion-title>
         </ion-toolbar>
       </ion-header>
     
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <strong>What is your first task of the day?</strong>
+        <p>Start with a cup of tea or water.</p>
       </div>
     </ion-content>
   </ion-page>
